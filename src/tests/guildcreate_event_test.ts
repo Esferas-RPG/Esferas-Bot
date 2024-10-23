2cdf633e929188b45a2d0958976df03a722f34cb
Luanderson Mendes <contato@luanderson.dev.br>
2024-10-23 13:37:04 -0400

    updated project

--- src/tests/guildcreate_event_test.ts
+++ src/tests/guildcreate_event_test.ts
@@ -1,6 +1,6 @@
 import {PermissionFlagsBits, type CommandInteraction, type Guild, type GuildMemberRoleManager} from "discord.js";
-import type {Client} from 'discordx';
-import { Discord, Slash } from "discordx";
+import type {Client, SimpleCommandMessage} from 'discordx';
+import { Discord, SimpleCommand, Slash } from "discordx";
 import {injectable} from 'tsyringe';
 
 @Discord()
@@ -8,28 +8,26 @@ import {injectable} from 'tsyringe';
 export class TestComnands {
     private allowedRoleId = '1294142903345479701'
 
-    @Slash({
+    @SimpleCommand({
         name: 'test_guild',
-        dmPermission: false,
         description: "Testa o evento de entrada na guilda",
-        defaultMemberPermissions: PermissionFlagsBits.Administrator,
     })
-    async testGuild(interaction: CommandInteraction, client: Client): Promise<void> {
-        const member = interaction.member?.roles as GuildMemberRoleManager;
+    async testGuild(command: SimpleCommandMessage, client: Client): Promise<void> {
+        const member = command.message.member?.roles as GuildMemberRoleManager;
         const hasRole = member?.cache.has(this.allowedRoleId);
 
         if (!hasRole) {
-            await interaction.reply("Você não tem permissão para usar este comando.");
+            //await command.message.reply("Você não tem permissão para usar este comando.");
             return;
         }
 
-        const guild: Guild | undefined = client.guilds.cache.get(interaction.guildId ?? "");
+        const guild: Guild | undefined = client.guilds.cache.get(command.message.guildId ?? "");
 
         if (guild) {
             client.emit('guildCreate', guild);
-            await interaction.reply(`Simulado a entrada na guilda: ${guild.name}`);
+            //await command.message.reply(`Simulado a entrada na guilda: ${guild.name}`);
         }else {
-            await interaction.reply(`Guilda não encontrada.`)
+            //await command.message.reply(`Guilda não encontrada.`)
         }
     }
 }
