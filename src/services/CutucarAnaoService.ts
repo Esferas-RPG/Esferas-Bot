19e3c62bddfb1c8db2f9b15e5d969a9c8aa3c2bc
Alexandre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2025-03-18 07:57:43 +0000

    Update CutucarAnaoService.ts
    potrra

--- src/services/CutucarAnaoService.ts
+++ src/services/CutucarAnaoService.ts
@@ -6,38 +6,47 @@ import * as crypto from 'crypto';
 @injectable()
 export class CutucarAnaoService {
 	async cutucarAnao(interaction: CommandInteraction) {
-		const channel = interaction.channel as TextChannel;
-		
-		const message: readonly string[] = [
-			`Não enche!`,
-			`Vai cutucar o cu do outro`,
-			`Cara, eu vou te bater.`,
-			`Para`,
-			`VAI TOMAR NO CU`
-		 ];
 		const choose = <T>(arr: readonly T[]): T => {
-			 const randomIndex = Math.floor(Math.random() * arr.length);
-			return arr[randomIndex];
+		    if (arr.length === 0) {
+		        throw new Error("O array está vazio e não é possível escolher um elemento.");
+		    }
+		    return arr[crypto.randomInt(0, arr.length)];
 		};
-										           
-		//let randomIndex = Math.floor(Math.random()*message.length);
-
-		interaction.reply('Você cutucou o anão!');
-
-		const webhooks = await channel.fetchWebhooks();
-		let webhook = webhooks.find((wh) => wh.name === 'Vechter Zanker');
-
-		if (!webhook) {
-			webhook = await channel.createWebhook({
-				name: 'Vechter Zanker',
-				avatar: 'https://i.imgur.com/CgBksj0.png',
+		try{
+			const channel = interaction.channel as TextChannel;
+			
+			const message: readonly string[] = [
+				`Não enche!`,
+				`Vai cutucar o cu do outro`,
+				`Cara, eu vou te bater.`,
+				`Para`,
+				`VAI TOMAR NO CU`
+			 ];
+			
+											           
+			
+	
+			interaction.reply('Você cutucou o anão!');
+	
+			const webhooks = await channel.fetchWebhooks();
+			let webhook = webhooks.find((wh) => wh.name === 'Vechter Zanker');
+	
+			if (!webhook) {
+				webhook = await channel.createWebhook({
+					name: 'Vechter Zanker',
+					avatar: 'https://i.imgur.com/CgBksj0.png',
+				});
+			}
+	
+			await webhook.send({
+				content: choose(messages),
 			});
+	
+			await webhook.delete();
+		}catch(error)
+		{
+			console.error('Erro ao cutucar o anão: ',error);
+			await interaction.reply('**O anão está puto e não te responde**');
 		}
-
-		await webhook.send({
-			content: choose(messages),
-		});
-
-		await webhook.delete();
 	}
 }
