93ab3e52176913048b32d7a669f9057b298bbbb9
Alexandre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2025-03-18 07:10:51 +0000

    Update CutucarAnaoService.ts
    update do token com link do Ingur

--- src/services/CutucarAnaoService.ts
+++ src/services/CutucarAnaoService.ts
@@ -15,7 +15,7 @@ export class CutucarAnaoService {
 		if (!webhook) {
 			webhook = await channel.createWebhook({
 				name: 'Vechter Zanker',
-				avatar: 'https://media.discordapp.net/attachments/1348313985270681712/1351257220712763513/Token_Zanker.png?ex=67d9b7da&is=67d8665a&hm=8043dbfd53c70c51479a34aa6f3701b98b28c0a311c0b1d7cfc4898a69a34e10&=&format=webp&quality=lossless',
+				avatar: 'https://i.imgur.com/CgBksj0.png',
 			});
 		}
 
